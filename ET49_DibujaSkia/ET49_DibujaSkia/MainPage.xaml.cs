8524df131f7dd5dff62b26d53a8ac8a8df885b3e
Francisco <frapagis@hotmail.com>
2018-05-19 15:58:20 +0200

    Add project files.

---
+++ ET49_DibujaSkia/ET49_DibujaSkia/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ET49_DibujaSkia
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
