f01acc0217b167edea7fff468d0d4873f313181c
Francisco <frapagis@hotmail.com>
2018-06-02 15:21:15 +0200

    init

--- ET49_DibujaSkia/ET49_DibujaSkia/MainPage.xaml.cs
+++ ET49_DibujaSkia/ET49_DibujaSkia/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +15,26 @@ namespace ET49_DibujaSkia
 		{
 			InitializeComponent();
 		}
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Red.ToSKColor(),
+                StrokeWidth = 25
+            };
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+
+            paint.Style = SKPaintStyle.Fill;
+            paint.Color = SKColors.Blue;
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, 50, paint);
+        }
 	}
 }
