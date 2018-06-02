f2a7b02a33034cbb9293f9493b27d1e9770e93c7
Francisco <frapagis@hotmail.com>
2018-06-02 15:45:35 +0200

    both

--- ET49_DibujaSkia/ET49_DibujaSkia/MainPage.xaml.cs
+++ ET49_DibujaSkia/ET49_DibujaSkia/MainPage.xaml.cs
@@ -16,7 +16,7 @@ namespace ET49_DibujaSkia
         Stopwatch temporizador = new Stopwatch();
         bool paginaActiva;
         float tick;
-        SKPaint paint = new SKPaint
+        SKPaint paintAnimation = new SKPaint
         {
             Style = SKPaintStyle.Stroke
         };
@@ -62,29 +62,29 @@ namespace ET49_DibujaSkia
 
 
             // Ejemplo simple
-            //SKPaint paint = new SKPaint
-            //{
-            //    Style = SKPaintStyle.Stroke,
-            //    Color = Color.Red.ToSKColor(),
-            //    StrokeWidth = 25
-            //};
-            //canvas.DrawCircle(info.Width / 2, info.Height / 4, 100, paint);
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Red.ToSKColor(),
+                StrokeWidth = 25
+            };
+            canvas.DrawCircle(info.Width / 2, info.Height / 4, 100, paint);
 
-            //paint.Style = SKPaintStyle.Fill;
-            //paint.Color = SKColors.Blue;
-            //canvas.DrawCircle(info.Width / 2, info.Height / 4, 50, paint);
+            paint.Style = SKPaintStyle.Fill;
+            paint.Color = SKColors.Blue;
+            canvas.DrawCircle(info.Width / 2, (info.Height / 4), 50, paint);
 
             // Animación simple
-            SKPoint center = new SKPoint(info.Width / 2, info.Height / 2);
+            SKPoint center = new SKPoint(info.Width / 2, (info.Height / 3) * 2);
             float baseRadius = Math.Min(info.Width, info.Height) / 12;
 
             for (int circle = 0; circle < 5; circle++)
             {
                 float radius = baseRadius * (circle + tick);
 
-                paint.StrokeWidth = baseRadius / 2 * (circle == 0 ? tick : 1);
-                paint.Color = new SKColor(0, 0, 255, (byte)(255 * (circle == 4 ? (1 - tick) : 1)));
-                canvas.DrawCircle(center.X, center.Y, radius, paint);
+                paintAnimation.StrokeWidth = baseRadius / 2 * (circle == 0 ? tick : 1);
+                paintAnimation.Color = new SKColor(0, 0, 255, (byte)(255 * (circle == 4 ? (1 - tick) : 1)));
+                canvas.DrawCircle(center.X, center.Y, radius, paintAnimation);
             }
         }
 	}
